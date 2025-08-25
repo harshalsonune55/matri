abf0dc2850b4eb14dcda5c60797a2996f1a0db42
harshalsonune55 <harshalsonune55@gmail.com>
2025-08-26 00:19:53 +0530

    some minor changes

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "/views"));
 // app.use('/auth', authRoutes);
 app.use(cors({
-  origin: "*", // or restrict to your frontend domain
+  origin: "https://ansh-op.onrender.com", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
   }));
